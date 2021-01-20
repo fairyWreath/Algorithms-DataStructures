302642ed2e09697721e62010aaa573a03ec9e9bd
dwihandi <nerradfour@gmail.com>
2021-01-20 13:49:36 +0700

    Added trees

---
+++ Trees/max_depth_binary.cpp
@@ -0,0 +1,22 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+ 
+
+    // recursive implementation
+int maxDepth(TreeNode* root) {
+    if (root == nullptr) return 0;
+
+    int maxLeft = maxDepth(root->left);         
+    int maxRight = maxDepth(root->right);
+    if (maxLeft > maxRight)
+        return maxLeft+1;
+    else
+        return maxRight+1;
+    
+}
